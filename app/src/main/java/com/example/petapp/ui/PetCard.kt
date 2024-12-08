0090d49beaf37e3b2f6e4cd6b8fa9452b6c0c819
MurtaghLuke <l.murtagh2m@gmail.com>
2024-12-08 20:01:25 +0000

    create petcard function and include animation for when card is expanded.

--- app/src/main/java/com/example/petapp/ui/PetCard.kt
+++ app/src/main/java/com/example/petapp/ui/PetCard.kt
@@ -1,44 +1,22 @@
 package com.example.petapp.ui
-
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.example.petapp.model.Pet
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
 
+//petcard composable function
 @Composable
-fun PetCard(pet: Pet) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-    ) {
-        Column {
-            Image(
-                painter = rememberImagePainter(data = pet.imageUrl),
-                contentDescription = "${pet.name}'s image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(120.dp),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = pet.name,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = "Calories Burned: ${pet.caloriesBurned}",
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+fun PetCard(pet: Pet, navController: NavController) {
+    val isExpanded = remember {mutableStateOf(false)} ////if card is expanded then the animation occurs
+
+    val animatedElevation by animateDpAsState(
+        targetValue = if (isExpanded.value) 12.dp else 8.dp,
+        /////tween specifies an animation with a duration of 300 milliseconds
+        animationSpec = tween(durationMillis = 300)
+    )
 }
